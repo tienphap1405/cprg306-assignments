d9cd459cc7b55d5df8d9d5939849d6f421cfa1d5
tienphap1405 <tienphap1405@gmail.com>
2024-09-16 13:17:54 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Page() {
   return(
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href={"/week-2"}>sdsd</Link>
+      <Link href={"/week-2"}>Link to week-2 page</Link>
     </div>
   );
 }

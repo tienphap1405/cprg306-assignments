bf3339aaea223f2c3212d3fb05819b4b75ce6ff4
tienphap1405 <tienphap1405@gmail.com>
2024-09-16 13:17:22 -0600

    Update student-info, pages

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function StudentInfo(){
+    return (
+        <div>
+            <p>Tien Phap (Evan) Nguyen</p>
+            <Link href={"https://github.com/tienphap1405/cprg306-assignments"}>Github Link</Link>
+        </div>
+    );
+}

c3d2912558a88dffd5874c6395bab20d469c83a1
tienphap1405 <tienphap1405@gmail.com>
2024-10-09 15:39:02 -0600

    Week-5 Submission

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ export default function Page() {
       <p><Link href={"/week-2"}>Link to week-2 page</Link></p>
       <p><Link href={"/week-3"}>Link to week-3 page</Link></p>
       <p><Link href={"/week-4"}>Link to week-4 page</Link></p>
+      <p><Link href={"/week-5"}>Link to week-5 page</Link></p>
     </div>
   );
 }
